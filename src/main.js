256f5d56772ff1f6f12afe58320b9bf305f9cdad
kkon2075 <lone8877@gmail.com>
2023-04-17 11:40:30 +0800

    first commit

---
+++ src/main.js
@@ -0,0 +1,30 @@
+/**
+ * main.js
+ *
+ * Bootstraps Vuetify and other plugins then mounts the App`
+ */
+
+// Components
+import App from './App.vue'
+
+// Composables
+import { createApp } from 'vue'
+
+// Plugins
+import { registerPlugins } from '@/plugins'
+
+// PWA
+import { registerSW } from 'virtual:pwa-register'
+
+import './main.sass'
+
+registerSW({
+  onNeedRefresh () {},
+  onOfflineReady () {}
+})
+
+const app = createApp(App)
+
+registerPlugins(app)
+
+app.mount('#app')
